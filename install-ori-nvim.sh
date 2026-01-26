d7e1912237cff36e8f6d51806463ad91fb8eb43d
orichain <dhaniprg.2007@gmail.com>
2026-01-26 14:44:26 +0700

    Add rockylinux

--- install-ori-nvim.sh
+++ install-ori-nvim.sh
@@ -9,8 +9,56 @@ rm -rf ~/.config/nvim \
   ~/.local/state/nvim \
   ~/.cache/nvim
 
-echo "==> Installing packages..."
-doas pkg_add neovim unzip luarocks-lua51 git bear spdlog clang-tools-extra xclip
+OS_TYPE=$(uname -s)
+case "$OS_TYPE" in
+    OpenBSD|FreeBSD|NetBSD)
+        DISTRO_ID=$(echo "$OS_TYPE" | tr '[:upper:]' '[:lower:]')
+        PKG_MGR="doas pkg_add"
+        ;;
+    Linux)
+        if [ -f /etc/os-release ]; then
+            . /etc/os-release
+            DISTRO_ID=$ID
+        else
+            DISTRO_ID="unknown_linux"
+        fi
+        if [ "$DISTRO_ID" = "rocky" ] || [ "$DISTRO_ID" = "fedora" ] || [ "$DISTRO_ID" = "rhel" ]; then
+            PKG_MGR="doas dnf"
+        elif [ "$DISTRO_ID" = "ubuntu" ] || [ "$DISTRO_ID" = "debian" ]; then
+            PKG_MGR="sudo apt-get"
+        fi
+        ;;
+    *)
+        DISTRO_ID="unknown"
+        ;;
+esac
+echo "==> Target System: $DISTRO_ID"
+if [ "$DISTRO_ID" = "openbsd" ]; then
+    echo "==> Configuring for OpenBSD..."
+    doas pkg_add neovim unzip luarocks-lua54 git bear spdlog clang-tools-extra-21.1.2 llvm-21.1.2p0 xclip
+    doas rm -f /usr/local/bin/luarocks /usr/local/bin/luarocks-admin /usr/local/bin/lua
+    doas ln -sf /usr/local/bin/luarocks-5.4 /usr/local/bin/luarocks
+    doas ln -sf /usr/local/bin/luarocks-admin-5.4 /usr/local/bin/luarocks-admin
+    doas ln -sf /usr/local/bin/lua54 /usr/local/bin/lua
+elif [ "$DISTRO_ID" = "rocky" ]; then
+    echo "==> Configuring for Rocky..."
+    sudo dnf -y install unzip luarocks git spdlog clang-tools-extra clang llvm xclip
+    curl -LO https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-linux-x86_64.tar.gz
+    tar -xzf nvim-linux-x86_64.tar.gz
+    rm nvim-linux-x86_64.tar.gz
+    sudo rm -rf /usr/local/lib/nvim-dist
+    sudo mv nvim-linux-x86_64 /usr/local/lib/nvim-dist
+    sudo chmod +x /usr/local/lib/nvim-dist
+    sudo ln -sf /usr/local/lib/nvim-dist/bin/nvim /usr/local/bin/nvim
+    curl -L https://github.com/fcying/compiledb-go/releases/download/v1.5.2/compiledb-linux-amd64.txz -o compiledb.txz
+    tar -xvf compiledb.txz
+    rm compiledb.txz
+    sudo mv compiledb /usr/local/bin
+    sudo chmod +x /usr/local/bin/compiledb
+else
+    echo "Not Automaticly Support For Distro [$DISTRO_ID]."
+    exit 1
+fi
 
 echo "==> Installing Nerd Font (0xProto)..."
 mkdir -p ~/.local/share/fonts
@@ -18,12 +66,6 @@ curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/0xProt
 unzip -q 0xProto.zip -d ~/.local/share/fonts/0xProto
 rm 0xProto.zip
 fc-cache -fv
-
-echo "==> Fixing Lua/Luarocks symlinks..."
-doas rm -f /usr/local/bin/luarocks /usr/local/bin/luarocks-admin /usr/local/bin/lua
-doas ln -sf /usr/local/bin/luarocks-5.1 /usr/local/bin/luarocks
-doas ln -sf /usr/local/bin/luarocks-admin-5.1 /usr/local/bin/luarocks-admin
-doas ln -sf /usr/local/bin/lua51 /usr/local/bin/lua
 
 echo "==> Installing Neovim config..."
 git clone --depth 1 https://github.com/orichain/orinvim.git ~/.config/nvim
@@ -33,4 +75,3 @@ echo "==> Syncing plugins (please wait)..."
 nvim --headless "+Lazy! sync" +qa
 echo "==> Done. Launching nvim..."
 exec nvim
-
